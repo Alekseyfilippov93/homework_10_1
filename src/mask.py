193e2b2bbf9ba95263f07a106d7429e0a9a80ea0
Aleksey40000 <alekseyfilippov9392@gmail.com>
2024-12-30 00:18:22 +0700

    update mask.py and widget.py

--- src/mask.py
+++ src/mask.py
@@ -1,14 +1,20 @@
+from typing import Union
+
+
 # Функция принимающая номер карты, и возврашает ее маску
-def get_mask_card_number(numbers: str) -> str:
+def get_mask_card_number(numbers: Union[str]) -> Union[str]:
     """Принимает на вход номер карты в виде числа и возвращает маску номера по правилу XXXX XX** **** XXXX"""
-    numbers = str(numbers)
-    new_mask_card = numbers[:4] + " " + numbers[6:8] + "** **** " + numbers[-4:]
+    new_mask_card = numbers[0:-12] + " " + numbers[11:13] + "** **** " + numbers[-4:]
     return new_mask_card
 
 
 # Функцию маскировки номера банковского счета
-def get_mask_account(mask_account: str) -> str:
+def get_mask_account(mask_account: Union[str]) -> Union[str]:
     """Принимает на вход номер счета в виде числа и возвращает маску номера по правилу **XXXX"""
-    mask_account = str(mask_account)
-    new_mask_account = "**" + mask_account[-4:]
+    new_mask_account = mask_account[:4] + " " + "**" + mask_account[-4:]
     return new_mask_account
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("maestro 8990922113665229"))
+    print(get_mask_account("счет 64686473678894779589"))
--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,9 @@
-# Функция принимает на вход строку формата Visa Platinum 7000792289606361, или Maestro 7000792289606361,
-# или Счет 73654108430135874305
-from mask import get_mask_account, get_mask_card_number
+from mask import get_mask_card_number, get_mask_account
 from typing import Union
 
 
+# Функция принимает на вход строку формата Visa Platinum 7000792289606361, или Maestro 7000792289606361,
+# или Счет 73654108430135874305
 def mask_account_card(card: Union[str]) -> str:
     """Определяем счет или номер карты и тип самой карты"""
     if "Visa" or "Maestro" or "MasterCard" in card:
@@ -12,6 +12,6 @@ def mask_account_card(card: Union[str]) -> str:
         return get_mask_account(card)
 
 
-card = "Счет 73654108430135874305"
-masked_info = mask_account_card(card)
-print(masked_info)
+if __name__ == "__main__":
+    print(mask_account_card("Visa 8990922113665229"))
+    print(get_mask_account("Счет 64686473678894779589"))
