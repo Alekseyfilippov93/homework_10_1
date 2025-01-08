6c1132d87bb56bcedd84ecff394104bddb9e729a
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-01-08 22:01:19 +0700

    init

--- src/mask.py
+++ src/mask.py
@@ -11,10 +11,5 @@ def get_mask_card_number(numbers: Union[str]) -> Union[str]:
 # Функцию маскировки номера банковского счета
 def get_mask_account(mask_account: Union[str]) -> Union[str]:
     """Принимает на вход номер счета в виде числа и возвращает маску номера по правилу **XXXX"""
-    new_mask_account = mask_account[:4] + " " + "**" + mask_account[-4:]
+    new_mask_account = "**" + mask_account[-4:]
     return new_mask_account
-
-
-if __name__ == "__main__":
-    print(get_mask_card_number("maestro 8990922113665229"))
-    print(get_mask_account("счет 64686473678894779589"))
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,17 @@
-from mask import get_mask_card_number, get_mask_account
 from typing import Union
+from src.mask import get_mask_account
+from src.mask import get_mask_card_number
 
 
 # Функция принимает на вход строку формата Visa Platinum 7000792289606361, или Maestro 7000792289606361,
 # или Счет 73654108430135874305
 def mask_account_card(card: Union[str]) -> str:
     """Определяем счет или номер карты и тип самой карты"""
-    if "Visa" or "Maestro" or "MasterCard" in card:
-        return get_mask_card_number(card)
-    elif "Счет" in card:
-        return "Счет" + "**" + get_mask_account(card[5:0])
+    card_split = card.split()
+    if "счет" in card.lower():
+        return f"{card_split[0]} {get_mask_account(card_split[-1])}"
+    else:
+        return f"{' '.join(card_split[:-1])} {get_mask_card_number(card_split[-1])}"
 
 
 # Функция принимает дату и преобразовывает её
@@ -18,9 +20,3 @@ def get_date(date_str: str) -> str:
     "ДД.ММ.ГГГГ"("11.03.2024")."""
     date_new = date_str[8:10] + "." + date_str[5:7] + "." + date_str[0:4]
     return date_new
-
-
-if __name__ == "__main__":
-    print(mask_account_card("Visa 8990922113665229"))
-    print(get_mask_account("Счет 64686473678894779589"))
-    print(get_date("2024-03-11T02:26:18.671407"))
