2450ddf0fb36c73a0b9e3d86f3c2d818c18b474b
Aleksey40000 <alekseyfilippov9392@gmail.com>
2025-01-27 02:07:38 +0700

    update

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,8 @@
+from typing import Any
+
+
 # Функция принимает на вход список словарей, представляющих транзакции
-def filter_by_currency(transactions, currency):
+def filter_by_currency(transactions: list, currency: str = "USD") -> Any:
     """Функция фильтрует транзакции по заданной валюте.Transactions: Список словарей, представляющих транзакции.
     currency: Валюта, по которой будет происходить фильтрация.
     """
@@ -22,10 +25,37 @@ transactions = [
         "id": 142264268,
         "state": "EXECUTED",
         "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "EUR"}},
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
         "description": "Перевод со счета на счет",
         "from": "Счет 19708645243227258542",
         "to": "Счет 75651667383060284188",
+    },
+    {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    },
+    {
+        "id": 895315941,
+        "state": "EXECUTED",
+        "date": "2018-08-19T04:27:37.904916",
+        "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод с карты на карту",
+        "from": "Visa Classic 6831982476737658",
+        "to": "Visa Platinum 8990922113665229",
+    },
+    {
+        "id": 594226727,
+        "state": "CANCELED",
+        "date": "2018-09-12T21:27:25.241689",
+        "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Visa Platinum 1246377376343588",
+        "to": "Счет 14211924144426031657",
     },
 ]
 
@@ -45,7 +75,6 @@ def transaction_descriptions(transactions):
 descriptions = transaction_descriptions(transactions)
 for transaction in range(2):
     print(next(descriptions))
-    pass
 
 
 # Функция генерирующая номер карты в формате XXXX XXXX XXXX XXXX
